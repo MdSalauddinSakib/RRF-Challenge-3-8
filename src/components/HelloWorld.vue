1329e595445208865e118838a341cfcbb4092ba9
Salauddin Sakib <salauddinsakib99@gmail.com>
2025-06-01 19:04:31 +0600

    navbar create done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,51 @@
 <template>
   <!-- Header Section -->
-  <header class="container">
-    <nav>
-      <!-- logo -->
-      <div class="container flex items-center gap-1.5 font-Roboto">
-        <img class="w-11" src="../assets/img/logo.png" alt="">
-        <div class="flex flex-col items-end">
-          <p class="text-lg font-bold">Plumber Point</p>
-          <p class="text-[#DD3142] font-bold text-sm leading-3">Company</p>
+  <header class="">
+    <nav class="bg-white shadow-xs">
+      <div class="container mx-auto flex justify-between items-center py-4 font-roboto">
+
+        <!-- logo -->
+        <div class="flex items-center gap-2">
+          <img class="w-11" src="../assets/img/logo.png" alt="">
+          <div class="flex flex-col items-end">
+            <p class="text-lg font-bold">Plumber Point</p>
+            <p class="text-[#DD3142] font-bold text-sm leading-tight">Company</p>
+          </div>
         </div>
+
+        <!-- menu -->
+        <ul class="flex items-center gap-6 text-sm font-medium">
+          <li>
+            <a class="nav" href="#">Home</a>
+          </li>
+          <li>
+            <a class="nav" href="#">About us</a>
+          </li>
+          <li>
+            <a class="nav" href="#">Services</a>
+          </li>
+          <li>
+            <a class="nav" href="#">Project</a>
+          </li>
+          <li>
+            <a class=" nav" href="#">Blogs</a>
+          </li>
+          <li class="">
+            <a class="nav" href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#"
+              class="bg-[#DD3142] text-white font-bold uppercase text-xs px-2 py-3 rounded-md hover:bg-red-600 transition">Request
+              a call Back</a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
+
+
+
 </template>
+
+<style scoped>
+</style>
